fced1476e3cdd0d603ce7c1bf934c9c0bcd7cd24
Buges Killian <killian.buges@ynov.com>
2022-04-29 19:48:04 +0200

    Up

--- Assets/Scenes/OctopathShader/scripts/playerController.cs
+++ Assets/Scenes/OctopathShader/scripts/playerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 public class playerController : MonoBehaviour
 {
 	Rigidbody rb;
-    public float speed = .5f;
+    public float speed = 1f;
     private Vector3 movement;
     private bool m_FacingRight = true;
     float velocity = 0f;
@@ -24,14 +24,21 @@ public class playerController : MonoBehaviour
         float inputX = Input.GetAxis("Vertical");
         float inputZ = Input.GetAxis("Horizontal");
 
-        Debug.Log(inputX);
+        
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 2f;
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 1f;
+        }
 
         movement = new Vector3(
             speed * -inputX,
             0,
             speed * inputZ);
-
-
 
         if (movement.z == 0)
         {
